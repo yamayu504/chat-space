a28357909832c4698b3fd7adb0b0323bb7bdd975
yamayu504 <yamayu.7349@gmail.com>
2019-01-06 16:09:30 +0900

    configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, key: [:user_name])
-    end
-
-
+   protected
+     def configure_permitted_parameters
+       #以下の:name部分は追加したカラム名に変える
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+     end
+     def after_sign_out_path_for(resouce)
+        new_user_session_path
+     end
 end
