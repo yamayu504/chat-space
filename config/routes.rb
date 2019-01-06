fdaf5058695a7327b95be26283f869e6f29a1dd9
yamayu504 <yamayu.7349@gmail.com>
2019-01-06 16:04:47 +0900

    Create routes
    #what
    基本的なルートを全て実装した。
    #why
    必要だったため

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  root  'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'messages' =>  'messages#index'
+  resources   :users,  only: [:edit, :update]
+  resources   :groups, only: [:new, :update, :edit, :create] do
+    resources :messages, only: [:new, :index]
+  end
 end
