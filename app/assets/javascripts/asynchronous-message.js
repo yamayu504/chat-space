16c925960a20af1be0e51767627a952c8477022a
yamayu504 <yamayu.7349@gmail.com>
2019-01-13 11:46:15 +0900

    Delete console log and stopPropagation()
    #what
    Console logの削除とstopPropagation()の削除
    #why
    開発用に必要なものであるため

--- app/assets/javascripts/asynchronous-message.js
+++ app/assets/javascripts/asynchronous-message.js
@@ -22,14 +22,12 @@ $(document).on('turbolinks:load', function() {
     }
     $('#new_message').on('submit', function(e){
       e.preventDefault();
-      e.stopPropagation();
       var formData = new FormData($(this).get(0));
       var href     = window.location.href;
       var messageConfirm  = $.isEmptyObject(formData.get("message[message]"));
       var imageConfirm    = $.isEmptyObject(formData.get("message[image]").name);
       // image,messageどちらも空の場合はこの時点で弾く。
       if( messageConfirm == true && imageConfirm == true){
-        console.log("空分岐の中に入っている。")
         alert('メッセージを入力してください。');
       }else{
         $.ajax({
@@ -41,27 +39,23 @@ $(document).on('turbolinks:load', function() {
           dataType:   'json',
           // 送る前に、disabledを追加する
           beforeSend: function(xhr, setting) {
-            console.log("送信前処理に入っている。")
             $(".container__main__footer__right__submit").prop('disabled', true);
           },
         })
         .done(function(data){
           // 正しく、遷移した場合の処理
-          console.log("doneに入ってる");
           var html = buildHTML(data);
           $('.container__main__contents').append(html);
           $('.container__main__footer__left__send').val('');
           $('#uploader-icon').val('');
-          // animate?
+          // animateによって、スクロールさせる処理
           $('.container__main__contents').animate({scrollTop: $('.container__main__contents')[0].scrollHeight}, 500, 'swing')
           // 処理が終わったら、data-disable-withを取り除いて、連続送信できるようにする。
           $(".container__main__footer__right__submit").prop('disabled', false);
         })
         // failは通信処理ができなかった時の処理
         .fail(function(XMLHttpRequest, textStatus, errorThrown){
-          console.log( textStatus );
           alert('通信に失敗しました。再度投稿してください。');
-          console.log("failに入ってる")
           // 処理が終わったら、data-disable-withを取り除いて、連続送信できるようにする。
           $(".container__main__footer__right__submit").prop('disabled', false);
         })
