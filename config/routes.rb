b621ea5756bc26eeae08d343e343bc2b83248a4a
yamayu504 <yamayu.7349@gmail.com>
2019-01-14 14:13:38 +0900

    Create Incremental search routes
    #what
    インクリメンタルサーチ のためのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root  'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources   :users,  only: [:edit, :update]
+  resources   :users,  only: [:index, :edit, :update]
   resources   :groups, only: [:new, :update, :edit, :create] do
     # new画面を必要としないため、createのみでよい。
     resources :messages, only: [:create, :index]
