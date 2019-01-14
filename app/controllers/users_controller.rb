12babb5cec7cb592708d09b0a50d7195e7eaa52a
yamayu504 <yamayu.7349@gmail.com>
2019-01-14 14:18:47 +0900

    Create index in user controller
    #what
    ユーザーコントローラの中にindexメソッドを作成した
    #why
    インクリメンタルサーチ のため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  def index
+    search_users     = User.where('user_name LIKE(?)', "%#{params[:keyword]}%")
+    # ここで本来ならばグループメンバーを弾くべきだが、editとnewが共存する_form.html.hamlでの実現方法がわからない。。。
+    search_users_ids = search_users.ids.select do |x|
+      x != current_user.id
+    end
+    @users = User.where(id: search_users_ids).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
