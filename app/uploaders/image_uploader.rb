78c0efaf2b338ec824b980da8674e786420ed373
yamayu504 <yamayu.7349@gmail.com>
2019-01-24 13:48:02 +0900

    Fix resize
    #what
    画像のサイズを小さくした。

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-    include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
-  process resize_to_fit: [800, 800]
+  process resize_to_limit: [300, 250]
   # Choose what kind of storage to use for this uploader:
   storage :fog
   # storage :fog
@@ -45,5 +45,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  process resize_to_fit: [800, 800]
 end
